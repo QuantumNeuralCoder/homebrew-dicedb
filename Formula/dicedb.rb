e85c8d8abf51ba7f9729b40e7ee0e17d2c04386e
QuantumNeuralCoder <163458725+QuantumNeuralCoder@users.noreply.github.com>
2025-06-02 11:44:03 -0700

    Add DiceDB CLI v1.0.8 Homebrew formula

--- Formula/dicedb.rb
+++ Formula/dicedb.rb
@@ -1,15 +1,20 @@
-class Dice < Formula
+class DiceDB < Formula
   desc "DiceDB engine – fast, reactive, in-memory database"
   homepage "https://github.com/DiceDB/dice"
   url "https://github.com/DiceDB/dice/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "1c613e44c8136d67dec73e92e084d38f39710d286fd1880998960891eb798a02"
   license "BSD-3-Clause"
 
-  depends_on "go" => :build
+  depends_on "go@1.23" => :build
 
   def install
     ENV["GOPROXY"] = "direct"
     ENV["GOSUMDB"] = "off"
+    ENV.prepend_path "PATH", Formula["go@1.23"].opt_bin
+
+    # Patch: create missing VERSION file
+    File.write("VERSION", "v1.0.0")
+
     system "go", "build", "-o", bin/"dicedb"
   end
 
